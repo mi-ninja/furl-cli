e23139501ffe6e117f77da8378a58dc4462b0776
Sudip Ghimire <ghimiresdp@gmail.com>
2026-05-29 16:26:39 +0000

    update cargo files and examples

--- examples/no-indicator/src/main.rs
+++ examples/no-indicator/src/main.rs
@@ -1,9 +1,9 @@
-use furl_core::{Downloader, GraphicalProgressReporter};
+use furl_core::Downloader;
 
 #[tokio::main]
 async fn main() {
     let url = "https://raw.githubusercontent.com/ghimiresdp/furl-cli/refs/heads/main/res/images/example.png";
-    let mut downloader = Downloader::new(url).with_reporter(GraphicalProgressReporter::new());
+    let mut downloader = Downloader::new(url);
     if let Ok(_) = downloader.download(".", None, Some(4)).await {
         println!("Download completed successfully!");
     } else {
